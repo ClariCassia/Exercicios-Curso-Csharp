7bc60b1cb2a95576b1836a21e01236836841e4b4
Clarissa de Cassia <clari.developer@gmail.com>
2024-04-24 18:07:40 -0300

    Add exercicio Calculo Nota Final

---
+++ Calculo-Nota-Final/Aluno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo_Nota_Final
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double NotaTrimestre1, NotaTrimestre2, NotaTrimestre3;
+
+        public double CalculaNotaFinal()
+        {
+            return NotaTrimestre1 + NotaTrimestre2 + NotaTrimestre3;
+        }
+
+        public string VerificaAprovacao(double notaFinal)
+        {
+            string resultado;
+
+            if (notaFinal >= 60)
+            {
+                resultado = "Aprovado";
+            }
+            else
+            {
+                resultado = "Reprovado";
+            }
+
+            return resultado;
+        }
+    }
+}
---
+++ Calculo-Nota-Final/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Globalization;
+
+namespace Calculo_Nota_Final
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+
+            Console.Write("Nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+
+            Console.Write("Nota 1º trimestre: ");
+            aluno.NotaTrimestre1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Nota 2º trimestre: ");
+            aluno.NotaTrimestre2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Nota 3º trimestre: ");
+            aluno.NotaTrimestre3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double notaFinal = aluno.CalculaNotaFinal();
+            string resultado = aluno.VerificaAprovacao(notaFinal);
+
+            Console.WriteLine($"Nota Final: {notaFinal.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"Status do Aluno: {resultado}");
+        }
+    }
+}
