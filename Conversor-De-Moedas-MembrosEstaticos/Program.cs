4adb1dd81635681d1358903b68a295d21e6c4964
Clarissa de Cassia <clari.developer@gmail.com>
2024-04-25 14:20:13 -0300

    Adicionado exercicio de Conversão de Moedas

---
+++ Conversor-De-Moedas-MembrosEstaticos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Conversor_De_Moedas_MembrosEstaticos
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Qual a cotação do dolar? ");
+            double cotacaoDolar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos dolares você vai comprar? ");
+            double qtdAcomprar = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double pagamentoEmReais = ConversorDeMoeda.DolarParaReal(qtdAcomprar, cotacaoDolar);
+
+            Console.Write($"Valor a ser pago em reais: {pagamentoEmReais.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+
+        private class ConversorDeMoeda
+        {
+            public static double Iof = 6.0;
+
+            public static double DolarParaReal(double quantia, double cotacao)
+            {
+                double total = quantia * cotacao;
+                return total + total * Iof / 100.0;
+            }
+        }
+    }
+}
