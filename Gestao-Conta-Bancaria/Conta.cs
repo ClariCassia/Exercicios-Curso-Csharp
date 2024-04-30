f55c7c7a719dbdd0d519ecb9d2e4a7b98f23a859
Clarissa de Cassia <clari.developer@gmail.com>
2024-04-30 11:54:37 -0300

    Add exercicio Gestão Conta Bancária

---
+++ Gestao-Conta-Bancaria/Conta.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace Gestao_Conta_Bancaria
+{
+    internal class Conta
+    {
+        public readonly double NumeroConta;
+        public string Titular { get; set; }
+
+        public double Saldo { get; private set; }
+
+        public Conta(double numeroConta, string nome)
+        {
+            NumeroConta = numeroConta;
+            Titular = nome;
+        }
+
+        public Conta(double numeroConta, string titular, double depositoInicial) : this(numeroConta, titular)
+        {
+            Depositar(depositoInicial);
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            Saldo += valorDeposito;
+        }
+
+        public double Sacar(double valorSaque)
+        {
+            double taxaSaque = 5.0;
+            if (Saldo >= valorSaque + taxaSaque)
+            {
+                Saldo -= (valorSaque + taxaSaque);
+                return Saldo;
+            }
+            else
+            {
+                Console.WriteLine("Saldo insuficiente para realizar o saque.");
+                Console.WriteLine();
+
+                return 0;
+            }
+        }
+
+        public override string ToString()
+        {
+            return ($"Conta: {NumeroConta}, Titular: {Titular} Saldo: R${Saldo.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
---
+++ Gestao-Conta-Bancaria/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace Gestao_Conta_Bancaria
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Conta conta1;
+
+            Console.Write("Digite o numero da conta: ");
+            double numeroConta = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Digite o nome do titular kda conta: ");
+            Console.Write("Digite o nome do titular kda conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial (s/n)? ");
+
+            char resposta = char.Parse(Console.ReadLine());
+
+            double depositoInicial = 0;
+
+            if (resposta == 's' || resposta == 'S')
+            {
+                Console.Write("Entre o valor de depósito inicial: ");
+                depositoInicial = double.Parse((Console.ReadLine()), CultureInfo.InvariantCulture);
+                conta1 = new Conta(numeroConta, titular, depositoInicial);
+            }
+            else
+            {
+                conta1 = new Conta(numeroConta, titular);
+            }
+
+            Console.WriteLine(conta1);
+
+            Console.Write("Entre com o valor do depósito: ");
+
+            conta1.Depositar(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine(conta1);
+
+            Console.Write("Entre um valor para saque: ");
+
+            conta1.Sacar(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+            Console.WriteLine(conta1);
+        }
+    }
+}
