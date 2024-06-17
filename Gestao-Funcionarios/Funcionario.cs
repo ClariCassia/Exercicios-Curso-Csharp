63310e95552cc83302f7532e06642b7367816248
Clarissa de Cassia <clari.developer@gmail.com>
2024-06-17 17:04:19 -0300

    :construction: add exercicios

---
+++ Gestao-Funcionarios/Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gestao_Funcionarios
+{
+    internal class Funcionario
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Funcionario(int id, string name, double salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+
+        public void SetSalary(double porcentagem)
+        {
+            Salary = CalculaAumento(porcentagem, Salary);
+        }
+
+        private static double CalculaAumento(double porcentagem, double salarioAtual)
+        {
+            return salarioAtual + (salarioAtual * (porcentagem / 100));
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Name}, {Salary.ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ Gestao-Funcionarios/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Gestao_Funcionarios
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Write("Quantos funcionários serão registrados: ");
+
+            int qtde = int.Parse(Console.ReadLine());
+
+            List<Funcionario> funcionarios = new List<Funcionario>();
+
+            for (int i = 1; i <= qtde; i++)
+            {
+                Console.WriteLine($"Emplyoee:{i}");
+                Console.WriteLine();
+
+                Console.Write("Id: ");
+                int id = int.Parse(Console.ReadLine());
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+
+                Console.Write("Salary: ");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                Console.WriteLine();
+
+                funcionarios.Add(new Funcionario(id, name, salary));
+            }
+
+            Console.Write("Digite o id: ");
+
+            int resposta = int.Parse(Console.ReadLine());
+
+            Console.Write("De quantos por cento sera o aumento: ");
+
+            double porcentagemAumento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (funcionarios.Exists(o => o.Id == resposta)) // Se existe
+                funcionarios.Find(o => o.Id == resposta).SetSalary(porcentagemAumento); // Busca
+            else
+                Console.WriteLine("Funcionario não encontrado");
+
+            #region Teste 1
+
+            //foreach (Funcionario func in funcionarios)
+            //{
+            //    if (func.Id == resposta)
+            //    {
+            //        func.SetSalary(porcentagemAumento);
+            //    }
+
+            //    Console.WriteLine(func.ToString());
+            //}
+
+            #endregion Teste 1
+
+            funcionarios.ForEach(o => Console.WriteLine(o.ToString()));
+        }
+    }
+}
