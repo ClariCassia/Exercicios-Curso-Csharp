8b9d6a2f02e92beb7a6648fc5f1bcdeeec749a5e
Clarissa de Cassia <clari.developer@gmail.com>
2024-06-17 17:15:30 -0300

    :construction: add exercicios

---
+++ AcelerandoNoConsole/Carro.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AcelerandoNoConsole
+{
+    public class Carro(string? modelo, string? montadora, string? marca, int ano, int potencia)
+    {
+        public string? Modelo = modelo;
+        public string? Montadora = montadora;
+        public string? Marca = marca;
+        public int Ano = ano;
+        public int Potencia = potencia;
+
+        public static void Acelerar(string marca)
+        {
+            Console.WriteLine($"Acelerando o {marca}..!!");
+        }
+
+        public static void PassandoDados(Carro car)
+        {
+            Console.WriteLine($"Modelo: {car.Modelo}");
+            Console.WriteLine($"Montadora: {car.Montadora}");
+            Console.WriteLine($"Ano: {car.Ano}");
+            Console.WriteLine($"Potência: {car.Potencia}");
+        }
+
+        public void Exibir()
+        {
+            Console.WriteLine();
+            PassandoDados(this);
+        }
+    }
+}
